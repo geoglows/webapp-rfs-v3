65d4e4ca1063cae6181063a65512a74bce47eb61
rileyhales <rileyhales1@gmail.com>
2026-07-22 12:23:20 -0600

    only use data location override in dev

--- src/rfsConfig.js
+++ src/rfsConfig.js
@@ -1,7 +1,7 @@
 import {configure} from "rfsjs/v3";
 const origin = () => (typeof location === "undefined" ? "http://localhost" : location.origin);
 const absolutize = value => (/^[a-z][a-z0-9+.-]*:/i.test(value) ? value : new URL(value, origin()).href);
-if (import.meta.env.VITE_RFS_V3_BASE) {
+if (import.meta.env.DEV && import.meta.env.VITE_RFS_V3_BASE) {
   configure({v3Base: absolutize(import.meta.env.VITE_RFS_V3_BASE)});
 }
 
